2170fca22d215da3729a3f7c6f7998a8552d7f87
abhijit <abhijitramteke345@gmail.com>
2024-02-15 20:18:35 +0530

    mycode

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Load Balancer Test</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f5;
+                color: #333;
+                text-align: center;
+                padding-top: 50px;
+            }
+            h1 {
+                color: #0066cc;
+                background-color: #ffff99;
+                padding: 10px;
+                border-radius: 8px;
+                display: inline-block;
+                border: 1px solid #cccccc;
+                box-shadow: 3px 3px 5px #888888;
+            }
+            .highlight {
+                color: #cc0000;
+                background-color: #ffff99;
+                padding: 5px;
+                border-radius: 5px;
+            }
+            a {
+                color: #cc0000;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>abhijitramteke.cloud</h1>
+        <img src"abhijit.jpg"
+        <p><span class="highlight">You are getting this webpage from <strong>$SERVER_NAME</strong></span></p>
+        <p>Powered by Abhijit Ramteke</p>
